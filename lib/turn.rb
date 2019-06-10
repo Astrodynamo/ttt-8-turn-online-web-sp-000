4079f6e5b5931c15ea735235096c91dc3c1d50d0
Amanda King <amapickell@gmail.com>
2019-06-10 22:23:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,8 +6,10 @@ def valid_move?
   return true if index.between?(0, 8) && position_taken?(board, index) == false
 end
 
-
-
+def position_taken? (board, index)
+  return false if board[index] == " " || board[index] == "" || board[index] == nil
+  true
+end
 
 def move
 
